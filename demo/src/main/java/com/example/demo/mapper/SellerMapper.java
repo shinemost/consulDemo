7a582d99ee0077c018d4e03b1302c51945cbeb00
shinemost <supertain147@163.com>
2024-02-27 01:53:39 +0000

    springbootDemo

--- demo/src/main/java/com/example/demo/mapper/SellerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import org.springframework.stereotype.Repository;
-import com.example.demo.entity.Seller;
-
-@Repository
-public interface SellerMapper {
-  Seller sel(int id);
-}
